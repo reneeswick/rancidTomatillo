a317c6ad101697a721323e9e940d1a7dea08112e
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-10-14 16:58:58 -0500

    Connect test data file to render correctly

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import logo from './logo.svg';
-import movieData from './movieData';
+import React, { Component } from 'react'
+import movieData from './movieData.js';
+import CardContainer from './CardContainer.js';
+import Card from './Card.js'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: movieData.movies,
+      greeting: 'hello'
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.greeting}!</h1>
+        {console.log(this.state.movies[1])}
+        <CardContainer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/CardContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class CardContainer extends Component {
+  render() {
+    return (
+      <div className="CardContainer">
+        <h1>Hello CardContainer!</h1>
+      </div>
+    );
+  }
+}
+
+
+
+export default CardContainer;
