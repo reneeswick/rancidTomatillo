7e40e1ae4fdfd2acb585c66cc92528040a2ea78f
Renee Swick-Ziller <reneeswick@gmail.com>
2021-10-15 12:16:33 -0600

    Added basic styling to movieDetails

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './MovieDetails.css';
 
 const MovieDetails = ({selectedMovie}) => {
-  console.log('MovieDetails props', selectedMovie.movie)
   return (
     <div>
       <h2>{selectedMovie.movie.title}</h2>
-        <img src= {selectedMovie.movie.backdrop_path} alt={selectedMovie.movie.title} />
+        <img className='movieBackDrop' src= {selectedMovie.movie.backdrop_path} alt={selectedMovie.movie.overview}/>
+        <p>{selectedMovie.movie.overview}</p>
+        <p>{selectedMovie.movie.release_date}</p>
     </div>
   )
 }
