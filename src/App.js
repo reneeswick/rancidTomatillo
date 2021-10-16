ad8e7f8e3036b7e9f595ee7bcce8dbd0c06a71aa
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-10-16 09:51:39 -0500

    Connect API fetch to the display

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import movieData from './movieData.js';
+// import movieData from './movieData.js';
 import CardContainer from './CardContainer.js';
 import MovieDetails from './MovieDetails.js';
 // import Card from './Card.js'
@@ -9,14 +9,24 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      movies: movieData.movies,
-      selectedMovie: {"movie": {id: 1, title: "Fake Movie Title", poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", release_date: "2019-12-04", overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", average_rating: 6, genres: ["Drama"], budget:63000000, revenue:100853753, runtime:139, tagline: "It's a movie!" }},
+      movies: [],
+      selectedMovie: { "movie": { id: 1, title: "Fake Movie Title", poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", release_date: "2019-12-04", overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", average_rating: 6, genres: ["Drama"], budget: 63000000, revenue: 100853753, runtime: 139, tagline: "It's a movie!" } },
       movieIsSelected: false
     }
   }
 
+  componentDidMount() {
+    fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
+      .then(results => results.json())
+      .then(data =>
+        this.setState({
+          movies: data.movies
+        })
+      )
+  }
+
   selectMovie = () => {
-    this.setState({movieIsSelected: true})
+    this.setState({ movieIsSelected: true })
   }
 
   returnHome = () => {
@@ -37,9 +47,9 @@ class App extends Component {
           <CardContainer
             movieData={this.state.movies}
             selectedMovie={this.state.selectedMovie}
-            movieIsSelected = {this.selectMovie}
-            />
-          }
+            movieIsSelected={this.selectMovie}
+          />
+        }
       </div>
     );
   }
