89ef5b2246f65dc327773dcbe8d81a80f62345f9
Renee Swick-Ziller <reneeswick@gmail.com>
2021-10-23 11:57:48 -0600

    Style the error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import '../styles/Error.css';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
   return (
     <div>
-      <h1>Hello world!</h1>
-      <p>This means our error page is working.</p>
+      <h1 className="error">Uh oh -- There's been a blooper!</h1>
+      <p className="error-msg">We're sorry. There's been an error.</p>
+      <img className="blooper-img" src="https://i.ytimg.com/vi/pvm6avc8Zwo/maxresdefault.jpg" alt= "multi-colored, vertical lines from classic blooper reels"/>
+      <p className="error-instr">Visit our home page or try again later.</p>
+      <Link to="/" className="link"> Click here to go home </Link>
     </div>
   )
 }
